8374d121a97d4226009dff8a3f08cf9c69a86b91
Rafael <a30115343@mcla.edu>
2023-10-04 23:22:37 -0400

    Bootstrap Carousel

---
+++ labs/bootstrap_carousel_lab/fonts/Test/CheckSize.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    var viewportWidth  = document.documentElement.clientWidth;
+
+    document.addEventListener(viewportWidth, function(){
+
+
+        let picone = document.getElementById("middle-img-one");
+        let pictwo = document.getElementById("middle-img-two");
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+});
---
+++ labs/bootstrap_carousel_lab/fonts/Test/test.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+//HEAD TAB LOGO IN TOP BORDER
+    document.getElementById("logo").innerHTML = "MyBrand";
+
+
+    
+
+
+//head SUB CATEGORIES
+
+   
+    
+
+
+
+
+    
+
+    
+
+
+
+
+
+});
