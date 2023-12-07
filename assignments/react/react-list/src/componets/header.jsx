af147d814908ee06a4efee54ba976e22cb7261c4
0xSilentScript <jalil24k@gmail.com>
2023-12-06 20:06:54 -0500

    list react

---
+++ assignments/react/react-list/src/componets/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../index.css";
+
+function Header() {
+  return (
+    <div>
+      <center>
+        <br />
+        <h1 id="style-title">Top 10 toy's of all time</h1>
+      </center>
+    </div>
+  );
+}
+
+export default Header;
